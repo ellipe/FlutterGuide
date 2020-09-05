1505229f14a431ff79da7c248f905bc7f3ddafd1
Luis Felipe Bertel <ellipe@gmail.com>
2020-09-05 10:57:13 -0500

    Column and functions...

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ void main() {
 }
 
 class MyPersonalApp extends StatelessWidget {
+  void answerQuestion(String question){
+    print('Answer chosen!: $question');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,7 +16,23 @@ class MyPersonalApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My Personal App'),
         ),
-        body: Text('This is my default text'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () => answerQuestion('one'),
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => answerQuestion('two'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => answerQuestion('three'),
+            ),
+          ],
+        ),
       ),
     );
   }
