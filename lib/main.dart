a92fe9608a536a6c6177884281954832a874ca52
Luis Felipe Bertel Mercado <luis.bertel@gorillalogic.com>
2020-09-05 09:51:10 -0500

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyPersonalApp());
+}
+
+class MyPersonalApp extends StatelessWidget{
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Home'));
+  }
+}
